f72a90a13e39f61971ac9b35cbeca46452389788
Raven Sears <ravenrosereynolds@gmail.com>
2021-12-10 14:31:49 +0000

    add methods and tests for full docking stations and empty docking stations

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,10 +4,12 @@ class DockingStation
     attr_reader :bike
     
     def release_bike
-        Bike.new
+        fail 'No bikes available' unless @bike
+        @bike
     end
 
     def dock(bike)
+        fail 'Docking Station Full' if @bike
         @bike = bike
     end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,18 +5,31 @@ describe DockingStation do
       it { is_expected.to respond_to :release_bike } 
     end
 
-    describe 'release_bike' do 
-      it 'releases a bike' do
-        station = DockingStation.new
-        expect(station.release_bike).to be_instance_of(Bike)
+    describe '#release_bike' do
+      it 'raises an error when there are no bikes available' do
+        subject = DockingStation.new
+        expect { subject.release_bike }.to raise_error 'No bikes available'
       end
     end
 
-    describe 'working?' do
-      it 'pull bike and check to see if working' do
+    describe '#release_bike' do 
+      it 'releases a bike' do
+        bike = Bike.new
+        subject.dock(bike)  #subject == DockingStation.new
+        expect(subject.release_bike).to eq bike
+      end
+    end
+
+    describe '#dock' do
+      it 'raises and error when full' do
+        subject.dock(Bike.new)
+        expect { subject.dock Bike.new}.to raise_error 'Docking Station Full'
+      end
+    end
+
+    it 'pull bike and check to see if working' do
       bike = Bike.new
       expect(bike.working?).to eq true
-      end
     end
 
     it 'docks something' do
